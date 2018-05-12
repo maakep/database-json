9a1449fcbc18f050ab9d9a2b4d31d827ac7e2541
Maakep <schon.fredrik@gmail.com>
2018-05-12 17:22:34 +0200

    Remove methods using asyncronous functions

--- src/index.ts
+++ src/index.ts
@@ -14,44 +14,16 @@ class Db {
         }
     }
 
-    /*public readAsync(callback: (content: any) => any): any {
-        fs.readFile(this.fname, "utf8", (err, data) => {
-            if (err)
-                throw err;
-
-            let object = JSON.parse(data);
-            callback(object);
-        });
-    }*/
-
     public read() {
         var file = fs.readFileSync(this.fname, "utf8");
         let object = JSON.parse(file);
         return object;
     }
 
-    /*public pushAsync(data: any, callback?: (err?: any) => any): void {
-        data = JSON.stringify(data);
-        fs.writeFile(this.fname, data, (err) => {
-            if (callback !== undefined) {
-                callback(err);
-            }
-        });
-    }*/
-
     public push(path: string, data: any): void {
         this.parent[path] = data;
         let insert = JSON.stringify(this.parent);
         fs.writeFileSync(this.fname, insert);
-    }
-
-    private update() {
-        this.insert(this.parent);
-    }
-
-    private insert(object: Object) {
-        var data = JSON.stringify(object);
-        fs.writeFileSync(this.fname, data);
     }
 
     public truncate() {
@@ -67,15 +39,18 @@ class Db {
     public unlink() {
         fs.unlinkSync(this.fname);
     }
-    
-    /*public deleteAsync(callback: (err?: any) => any) {
-        fs.unlink(this.fname, (err) => {
-            callback(err);
-        })
-    }*/
 
     public exists() {
         return fs.existsSync(this.fname);
+    }
+
+    private update() {
+        this.insert(this.parent);
+    }
+
+    private insert(object: Object) {
+        var data = JSON.stringify(object);
+        fs.writeFileSync(this.fname, data);
     }
 }
 
